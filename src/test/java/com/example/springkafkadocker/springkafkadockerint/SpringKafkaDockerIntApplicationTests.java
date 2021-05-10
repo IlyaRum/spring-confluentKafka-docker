deb3edb1b015bb68ce2023b03a075942ef9021e1
IlyaR <irumayncev@mail.ru>
2021-05-10 12:46:06 +0300

    add src for Consumer

---
+++ src/test/java/com/example/springkafkadocker/springkafkadockerint/SpringKafkaDockerIntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springkafkadocker.springkafkadockerint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaDockerIntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
